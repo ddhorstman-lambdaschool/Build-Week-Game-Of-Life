716a9654f8fc5ee0046f56d5c413043023219300
David Horstman <davidzilla27@gmail.com>
2020-07-28 15:13:44 -0400

    Re-add speed slider. Progress on Game of Life algorithm

--- src/components/GameOfLife.js
+++ src/components/GameOfLife.js
@@ -25,8 +25,41 @@ export default class GameOfLife extends React.Component {
   };
   toggleCell = (x, y) => {
     let { grid } = this.state;
-    grid[x][y] = !grid[x][y];
+    grid[x][y] = grid[x][y] ? 0 : 1;
     this.setState({ grid });
+  };
+  progressGame = (n = 1) => {
+    let startTime = Date.now();
+    let changes = {};
+    let { grid, side } = this.state;
+    for (let i = 0; i < side; i++) {
+      const left = i === 0 ? side - 1 : i - 1;
+      const right = i === side - 1 ? 0 : i + 1;
+      for (let j = 0; j < side; j++) {
+        const alive = grid[i][j];
+        const above = j === 0 ? side - 1 : j - 1;
+        const below = j === side - 1 ? 0 : j + 1;
+        const neighborSum =
+          grid[left][above] +
+          grid[left][below] +
+          grid[right][above] +
+          grid[right][below] +
+          grid[left][j] +
+          grid[right][j] +
+          grid[i][above] +
+          grid[i][below];
+        if (!alive && neighborSum === 3) {
+          changes[`${i},${j}`] = 1;
+        }
+        if (alive && (neighborSum < 2 || neighborSum > 3)) {
+          changes[`${i},${j}`] = 0;
+        }
+      }
+    }
+    for(let item in changes){
+      console.log(item)
+    }
+    console.log("Finished in", Date.now() - startTime, "milliseconds");
   };
   loadPreset = preset => {
     console.log(preset);
@@ -38,6 +71,7 @@ export default class GameOfLife extends React.Component {
           grid={this.state.grid}
           toggleCell={this.toggleCell}
           resetGrid={this.resetGrid}
+          progressGame={this.progressGame}
         />
         <Presets loadPreset={this.loadPreset} />
         <Rules />
--- src/components/game-of-life/PlayGrid.js
+++ src/components/game-of-life/PlayGrid.js
@@ -4,8 +4,14 @@ export default class PlayGrid extends React.Component {
   constructor(props) {
     super(props);
     let { color } = props;
-    this.state = { color, generation: 0, isPlaying: false };
+    this.state = { color, generation: 0, isPlaying: false, speed: 1 };
   }
+  togglePlaying = () => {
+    this.setState({ isPlaying: !this.state.isPlaying });
+  };
+  setSpeed = ({ target: { value } }) => {
+    this.setState({ speed: value });
+  };
   render() {
     return (
       <div className='PlayGrid'>
@@ -13,12 +19,27 @@ export default class PlayGrid extends React.Component {
         <Grid
           grid={this.props.grid}
           color={this.state.color}
-          toggleCell={this.props.toggleCell}
+          toggleCell={this.state.isPlaying ? () => {} : this.props.toggleCell}
         />
-        <div>
-          <button>{this.state.isPlaying ? "Pause" : "Play"}</button>
-          <button>Step Forward</button>
+        <div className="ButtonBar">
+          <div>
+          <button onClick={this.togglePlaying}>
+            {this.state.isPlaying ? "Pause" : "Play"}
+          </button>
+          <button onClick={this.props.progressGame}>Step Forward</button>
           <button onClick={this.props.resetGrid}>Reset</button>
+          </div>
+          <label>
+            {`${this.state.speed}x`}
+            <input
+              type='range'
+              min={1}
+              max={10}
+              value={this.state.speed}
+              onInput={this.setSpeed}
+              onChange={() => {}}
+            />
+          </label>
         </div>
       </div>
     );
